f10416ba21a413e1da3f30fed5a8c2b3e67b796a
Thomas Bella <thomas.bella@unterhaltungsbox.com>
2014-07-23 21:27:37 +0200

    Added hack protection

--- system/SQL.class.php
+++ system/SQL.class.php
@@ -11,6 +11,11 @@
  *
  */
 
+// Hack protection
+if (!defined("FW_PATH"))
+	exit(file_get_contents("../pages/hack.txt"));
+
+
 namespace UDFrameWork\system;
 use \PDO;
 use \PDOException;
--- system/Shutdown.class.php
+++ system/Shutdown.class.php
@@ -13,6 +13,11 @@
  *
  */
 
+// Hack protection
+if (!defined("FW_PATH"))
+    exit(file_get_contents("../pages/hack.txt"));
+
+
 namespace UDFrameWork\lib\system;
 
 class Shutdown
