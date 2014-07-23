1c2334390c97a6f4280a5374b83faa6c887576af
Thomas Bella <thomas.bella@unterhaltungsbox.com>
2014-07-23 21:15:50 +0200

    Added base of ErrorHandler

---
+++ system/ErrorHandler.class.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ *
+ *  Handles all errors
+ *
+ * @copyright	2014 Unterhaltungsbox.com
+ * @package	    com.unterhaltungsbox.udfw
+ * @subpackage	system
+ * @category 	UDFrameWork
+ * @version     1.0
+ * @author      Thomas2500
+ *
+ */
+
+namespace UDFrameWork\system;
+
+class ErrorHandler
+{
+
+    public function __construct()
+    {
+
+        ini_set( 'display_errors', 1 );
+        error_reporting( -1 );
+        set_error_handler( array( '\UDFrameWork\system\ErrorHandler', 'captureNormal' ), E_ALL );
+        set_exception_handler( array( '\UDFrameWork\system\ErrorHandler', 'captureException' ) );
+        register_shutdown_function( array( '\UDFrameWork\system\ErrorHandler', 'captureShutdown' ) );
+
+    }
+
+    public static function captureNormal( $number, $message, $file, $line )
+    {
+
+    }
+
+    public static function captureException( $exception )
+    {
+
+    }
+
+    public static function captureShutdown( )
+    {
+        $error = error_get_last( );
+    }
+}
