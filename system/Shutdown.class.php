9daa2b256599b51db363e2c84529c2f7546c7abc
Thomas Bella <thomas.bella@unterhaltungsbox.com>
2014-07-23 21:16:06 +0200

    Added base of ShutdownHandler

---
+++ system/Shutdown.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *
+ *  Shutdown (exit, die, timeout, finish) handler
+ *
+ * @copyright	2014 Unterhaltungsbox.com
+ * @package	    com.unterhaltungsbox.udfw
+ * @subpackage	system
+ * @category 	UDFrameWork
+ * @version     1.0
+ * @author      Thomas2500
+ *
+ */
+
+namespace UDFrameWork\lib\system;
+
+class Shutdown
+{
+
+    static public function Finish()
+    {
+        global $SQL;
+
+        if (function_exists("fastcgi_finish_request"))
+            fastcgi_finish_request();
+    }
+
+}
